52c7bff65f1463302dd9427b053864c3aceb167f
heryfitiavana22 <hheryfitiavana@gmail.com>
2022-09-20 15:25:54 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # get_username_github
+
+## get username github
+
+1) 
+```
+npm install
+```
+
+2) 
+```
+nodemon app
+```
---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express()
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use(express.json())
+
+const {Octokit} = require('@octokit/core');
+const octokit = new Octokit({auth : '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'});
+
+app.get('/', (request, response) => {
+    response.status(200).render('index')
+})
+
+app.post('/getUser', (request, response) => {
+    let country = request.body.country,
+        page = request.body.page || 1;
+    async function getUser() { 
+        let query = '?q=' + encodeURIComponent('location:'+country)+'&page='+page;
+        await octokit.request('GET /search/users'+query, {})
+            .then((value) => {
+                response.send({
+                    value : value,
+                    isError : false
+                })
+            })
+            .catch((err) => {
+                response.send({isError : true})
+                console.log(err)
+            })
+    }
+    getUser()
+})
+
+app.listen(3000)
